30429103f876cd40d626dd916656650331b33e96
kabagema <35719135+kabagema@users.noreply.github.com>
2021-01-20 00:13:32 +0200

    Update dbModel.js

--- tiktok-backend/dbModel.js
+++ tiktok-backend/dbModel.js
@@ -1,14 +1,16 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const tiktokSchema = mongoose.Schema({
-    url: String,
-    channel: String,
-    song: String,
-    likes: String,
-    messages: String,
-    description: String,
-    shares: String,
+  url: String,
+  channel: String,
+  song: String,
+  likes: String,
+  messages: String,
+  description: String,
+  shares: String,
 });
 
+// collection inside the databse
+export default mongoose.model("tiktokVideos", tiktokSchema);
 
 // 2: 30 : 02
