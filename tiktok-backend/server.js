e1302ad824459b9fbdf3deb4458ee3ba350c48b9
kabagema <35719135+kabagema@users.noreply.github.com>
2021-01-19 23:55:03 +0200

    tik tok changes in server

--- tiktok-backend/server.js
+++ tiktok-backend/server.js
@@ -2,12 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import Data from "./data.js";
 
-import Videos from "./dbModel"
+import Videos from "./dbModel.js"
 // app config
 const app = express();
 const port = 9000;
 
 //middlewares
+ app.use(express.json());
+ app.use((req, res, next)=> {
+   res.setHeaders("Access-Control-Allow-Origin","*"),
+   res.setHeaders("Access-Control-Allow-Headers", "*"),
+   next();
+ })
 
 //db config
 const connection_url =
@@ -24,12 +30,23 @@ app.get("/", (req, res) => res.status(200).send("hello big boss"));
 
 app.get("/v1/posts", (req, res) => res.status(200).send(Data));
 
+app.get("/v2/posts", (req, res) => {
+  Videos.find((err, data) =>{
+      if(err)
+      {
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(data);
+      }
+  });
+})
+
 app.post("/v2/posts", (req, res) => {
   // Post Request is to ADD DATA to the database
   // it will let us ADD a video DOCUMENT to the videos COLLECTIOn
     const dbVideos = req.body;
 
-    dbVideos.create(dbVideos, (err, data) => {
+    Videos.create(dbVideos, (err, data) => {
         if (err) {
         res.status(500).send(err);
         } else {
@@ -40,3 +57,6 @@ app.post("/v2/posts", (req, res) => {
 
 // listen
 app.listen(port, () => console.log(`listening on localhost:${port}`));
+
+
+//ignoring security
